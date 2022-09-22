2e067aea77b6a748f3afe506e8aa2bf5e38fe9ee
Acer <Acer@LAPTOP-OFFKC515>
2022-09-22 21:58:20 +0500

    Добавьте файлы проекта.

---
+++ pz_5/Program.cs
@@ -0,0 +1,36 @@
+﻿//for (int a = 20; a <= 90; a += 5)
+//{
+//    Console.WriteLine(a);
+//}
+
+
+//for (char a = 'Ж'; a <= 'M'; a++)
+//{
+//    Console.WriteLine(a);
+
+//}
+
+
+//for (var i = 0; i < 8; i++)
+//{
+//    for (var j = 0; j < 7; j++)
+//    {
+//        Console.Write('#');
+//    }
+//    Console.WriteLine();
+//}
+//Console.ReadKey();
+
+//for (int s = 0; s <= 100; s += 1)
+//{
+//    if (s % 13 == 0)
+//    {
+//        Console.WriteLine($"{s} ");
+//    }
+
+//}
+
+//for (int i = 5 , j = 100; j - i !=  9; i++ ,j --)
+//{
+//    Console.WriteLine(i + "   " + j );
+//}
